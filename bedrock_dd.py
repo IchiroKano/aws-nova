96b12fcaa5564a9710e69a2a015ddeb3bf710b17
ichirokano <ichiro.kano2@asahi.email.ne.jp>
2025-01-03 14:11:15 +0900

    AWS Bedrock with datadog LLM Observability

--- bedrock_dd.py
+++ bedrock_dd.py
@@ -1,15 +1,18 @@
-
 import sys
 import boto3
 import json
 from botocore.exceptions import ClientError
 
-# Bedrock runtime client の作成, Nova Lite モデルを使う。バージニア北部リージョンで利用可能 
+from ddtrace.llmobs import LLMObs # Datadog
+from ddtrace.llmobs.decorators import embedding, llm, retrieval, workflow # Datadog
+
+# Bedrock runtime client の作成, Nova Lite モデルを使う。バージニア北部リージョンで利用可能
 bedrockRuntime = boto3.client("bedrock-runtime", region_name="us-east-1")
 MODEL_ID = "amazon.nova-lite-v1:0"
 
-def runjob( prompt ):
-    # モデルのネイティブ構造を使ってリクエストする
+# モデルのネイティブ構造を使ってリクエストする
+@llm(model_name="nova-lite-v1:0", model_provider="amazon")
+def llm_call( prompt ):
     requestPayload = {
         "messages": [
             {
@@ -20,7 +23,7 @@ def runjob( prompt ):
             }
         ],
         "inferenceConfig": {
-            "maxTokens": 512, 
+            "maxTokens": 512,
             "stopSequences": [],
             "temperature": 0.7,
             "topP": 0.9
@@ -35,22 +38,34 @@ def runjob( prompt ):
             contentType = "application/json"
         )
 
-        # generated_text の取得と表示 
+        # generated_text の取得と表示
         responseBody = json.loads( response['body'].read() )
         generatedText = responseBody['output']['message']['content'][0]['text']
         print( generatedText )
+
+        LLMObs.annotate(
+            span=None,
+            input_data=[{"role": "user", "content": prompt}],
+            output_data=[{"role": "assistant", "content": generatedText}],
+            metadata={"temperature": 0.7, "max_tokens": 512},
+            tags={"host": "game.funnygeekjp.com"},
+        )
+
+        return response
 
     except (ClientError, Exception) as e:
         print(f"ERROR: 実行できません '{MODEL_ID}', Reason: {e}")
         exit(1)
 
 if __name__ == "__main__":
-    # プロンプト入力例
+    # コマンドラインからプロンプトを取得
     if len(sys.argv) < 2:
         print("書式$ python3 bedrock.py 'ここに質問をテキストで記載する'")
         sys.exit(1)
     else:
         prompt = sys.argv[1]
-        print( prompt )
-        runjob( prompt )
+        llm_call( prompt )
+
+
+
 
